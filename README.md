bfbab342008fdba848fc075280576789fe5ecc13
Vítor Spínola <87049969+spinolavitor@users.noreply.github.com>
2025-12-29 20:27:32 +0000

    Adicionando os scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone_tabnews
+
 Projeto do curso.dev com o objetivo de recriar o tabnews.com.br
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Desafio concluído com sucesso!</h1>
+  return <h1>Desafio concluído com sucesso!</h1>;
 }
 
 export default Home;
