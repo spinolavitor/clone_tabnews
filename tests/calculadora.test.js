d3aa8fd34db833f1bd151ba219c699cc4faa7f43
Vítor Spínola <87049969+spinolavitor@users.noreply.github.com>
2026-01-03 15:02:48 +0000

    Criamos a pasta `Tests`, o arquivo `calculadora.test.js` e testamos alguns códigos com `Arrow Functions`

---
+++ tests/calculadora.test.js
@@ -0,0 +1,22 @@
+// test("nome do teste", callbackFunction);
+
+// function callbackFunction() {
+//   console.log("Esta função está sendo chamada?");
+// }
+
+// test("nome do teste", function () {
+//   console.log("Esta função anônima continua funcionando?");
+// });
+
+test("Nome do teste", () => {
+  console.log("Esta Arrow Function está funcionando?");
+});
+
+test("Testando novamente", () => {
+  console.log("Testando novamente!");
+});
+
+test("Espero que 1 seja 1", () => {
+  expect(1).toBe(1);
+});
+// Esse é um exemplo de Hard Coding
