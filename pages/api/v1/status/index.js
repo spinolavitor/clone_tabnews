a3692da6be37a4901a39d92b62c828e577880454
Vítor Spínola <87049969+spinolavitor@users.noreply.github.com>
2026-01-21 03:29:02 +0000

    Aula 18 parcialmente concluída

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,10 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
   // response.status(200).send("Deu certo!");
+  // console.log(database);
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   response.status(200).json({ chave: "Deu certo" });
 }
 
