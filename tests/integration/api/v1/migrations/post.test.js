3558da64b37529a4c14290844d11616f1d39d566
Vítor Spínola <87049969+spinolavitor@users.noreply.github.com>
2026-02-12 07:12:56 +0000

    Enhance migration test to validate response body length
    Added a check to ensure response body contains items.

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,3 +1,11 @@
+import database from "infra/database";
+
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;")
+}
+
 test("POST to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST",
@@ -5,7 +13,8 @@ test("POST to /api/v1/migrations should return 200", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  console.log(responseBody);
+//  console.log(responseBody);
 
   expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
 });
