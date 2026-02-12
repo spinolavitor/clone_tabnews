63ff0d6bdf704bb75a4243d235c2e9dff7cdd917
Vítor Spínola <87049969+spinolavitor@users.noreply.github.com>
2026-02-12 10:25:55 +0000

    Consertando a morte do meu antigo Codespaces e commitando tudo que eu perdi

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -2,28 +2,31 @@ import migrationRunner from 'node-pg-migrate';
 import { join } from "node:path";
 
 export default async function migrations(request, response) {
-  if  (request.method === "GET") {
-    const migrations = await migrationRunner({
+  const defaultMigrationsOptions = {
       databaseUrl: process.env.DATABASE_URL,
       dryRun: true,
       dir: join("infra", "migrations"),
       direction: "up",
       verbose: true,
       migrationsTable: "pgmigrations",
-    });
-      return response.status(200).json(migrations);
+  };
+
+  if  (request.method === "GET") {
+    const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
+    return response.status(200).json(pendingMigrations);
   }
 
   if  (request.method === "POST") {
-    const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationsOptions,
       dryRun: false,
-      dir: join("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
     });
-      return response.status(200).json(migrations);
+
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);
+    }
+
+      return response.status(200).json(migratedMigrations);
   }
 
   return response.status(405).end();
