605679c3a8bc8f5de5f87c15db1ec1ba2cf57d7a
Vítor Spínola <87049969+spinolavitor@users.noreply.github.com>
2026-03-24 21:09:38 +0000

    feat: use `ServiceError` in `database.js`

--- infra/database.js
+++ infra/database.js
@@ -1,28 +1,21 @@
 import { Client } from "pg";
+import { ServiceError } from "./errors.js";
 
 console.log("NOD_ENV: " + process.env.NODE_ENV);
 
 async function query(queryObject) {
   let client;
-
-  //  console.log('Credenciais do Postgres:', {
-  //    host: process.env.POSTGRES_HOST,
-  //    port: process.env.POSTGRES_PORT,
-  //    user: process.env.POSTGRES_USER,
-  //    database: process.env.POSTGRES_DB,
-  //    password: process.env.POSTGRES_PASSWORD,
-  //  });
-
   try {
     client = await getNewClient();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
-    console.log("\n Erro dentro do catch do database.js:");
-    console.error(error);
-    throw error;
+    const serviceErrorObject = new ServiceError({
+      message: "Erro na conexão com o Banco ou na Query",
+      cause: error,
+    });
+    throw serviceErrorObject;
   } finally {
-    // "?" é um Optional Chaining
     await client?.end();
   }
 }
