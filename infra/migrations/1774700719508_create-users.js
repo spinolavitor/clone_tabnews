7d13046c63dc06734332d43e00bc01eec21e9646
Vítor Spínola <87049969+spinolavitor@users.noreply.github.com>
2026-03-28 13:23:26 +0000

    feat: add migration to create `users` table

---
+++ infra/migrations/1774700719508_create-users.js
@@ -0,0 +1,40 @@
+exports.up = (pgm) => {
+  pgm.createTable("users", {
+    id: {
+      // Universally unique identifier
+      type: "uuid",
+      primaryKey: true,
+      default: pgm.func("gen_random_uuid()"),
+    },
+    // For reference, GitHub limits username to 39 characters
+    username: {
+      type: "varchar(30)",
+      notNull: true,
+      unique: true,
+    },
+    // Why 254 in length? https://stackoverflow.com/a/1199238
+    email: {
+      type: "varchar(254)",
+      notNull: true,
+      unique: true,
+    },
+    // Why 60 in length? https://www.npmjs.com/package/bcrypt#hash-info
+    password: {
+      type: "varchar(60)",
+      notNull: true,
+    },
+    // Always use timestamp with timezone (tz) | https://justatheory.com/2012/04/postgres-use-timestamptz/
+    created_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("timezone('utc', now())"),
+    },
+    updated_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("timezone('utc', now())"),
+    },
+  });
+};
+
+exports.down = false;
